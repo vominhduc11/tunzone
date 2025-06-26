68d68ef41c3f1aff606b19c7ae1f0995855187d5
vominhduc11 <vominhduc760@gmail.com>
2025-06-26 18:32:53 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import '@/styles/globals.css';
-import Header from '@/components/layouts/Header';
-import Footer from '@/components/layouts/Footer';
+import Header from '../components/layouts/Header';
+import Footer from '../components/layouts/Footer';
 // import { ThemeProvider } from '@/context/themeContext';
 
 const geistSans = Geist({
